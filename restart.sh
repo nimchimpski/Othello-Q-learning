caf0467cbdeafdea288eebe2df6ef0e5a20c6675
nimchimpski <alxwebbuk@gmail.com>
2024-02-12 23:55:39 +0000

    ss

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script is used to restart the server
-sudo systemctl daemon-reload
-sudo systemctl start othello
-sudo systemctl enable othello
-sudo systemctl status othello
-```
